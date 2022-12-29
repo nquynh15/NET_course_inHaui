ef0f31aaa9c3d803919defd595f2e6eb129b1276
quynhquynh154 <nguyenquynh170820@gmail.com>
2022-12-29 22:21:18 +0700

    Second add

---
+++ B2/Bai2/Bai2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Mang
+{
+    public static void Main(string[] args)
+    {
+        int n;
+        int[] a;
+        
+        Console.Write("Emter the length of Array: ");
+        n = int.Parse(Console.ReadLine());
+        a= new int[n];
+        for(int i=0; i<n; i++)
+        {
+            Console.Write("arr[{0}] = ", i);
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        int max = a[0];
+        foreach(int x in a)
+        {
+            if (max < x) 
+                max = x; 
+        }
+        int min = a[0];
+        foreach(int x in a)
+        {
+            if(min > x) 
+                min= x; 
+        }
+        Console.WriteLine("Max = " + max);
+        Console.WriteLine("Min = " + min);
+    }
+}
---
+++ B2/Bai2/Chuoi/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Chuoi
+{
+    public static void Main(string[] args)
+    {
+        string s;
+
+        Console.Write("Emter you string: ");
+        s = Console.ReadLine();
+
+        Console.WriteLine(s);
+
+        var arr = s.Split(" ");
+        foreach(var i in arr)
+        {
+            Console.WriteLine(i);
+        }
+        Console.Write("Enter a characters:  ");
+        char x = char.Parse(Console.ReadLine()); ;
+        int c = 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == x)
+                c++;
+        }
+        Console.WriteLine(x+" appears "+c+" times in your string");
+    }
+}
