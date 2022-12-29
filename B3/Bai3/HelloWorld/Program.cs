a95e0dce72e3b36c03ae6222b40bdb76d1e0b2b2
quynhquynh154 <nguyenquynh170820@gmail.com>
2022-12-29 22:22:07 +0700

    Third add

---
+++ B3/Bai3/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var b = 24L / 5 == 24 / 5d;
+
+        var a;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(a = a == null ? "" : a);
+    }
+}
